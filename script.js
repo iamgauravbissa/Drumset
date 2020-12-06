5e56cc1e0e8131d1912c525ea91fe5814d4e6929
Gaurav Bissa <48169747+iamgauravbissa@users.noreply.github.com>
2020-12-06 22:00:36 +0530

    Drumset
    learn adding keyboard events

---
+++ script.js
@@ -0,0 +1,18 @@
+window.addEventListener('keydown',function(e){
+    if(e.keyCode==65){
+        var x = document.getElementById('65');
+        x.play();
+     }
+     else if(e.keyCode==66){
+        var x = document.getElementById('66');
+        x.play();
+     }
+     else if(e.keyCode==67){
+        var x = document.getElementById('67');
+        x.play();
+     }
+     else if(e.keyCode==68){
+        var x = document.getElementById('68');
+        x.play();
+     }
+});
