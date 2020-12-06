37351163c45233b3832e99ce1ea535575f2ff33a
Gaurav Bissa <48169747+iamgauravbissa@users.noreply.github.com>
2020-12-06 22:26:35 +0530

    updayte

--- script.js
+++ script.js
@@ -6,14 +6,17 @@ window.addEventListener('keydown',function(e){
         
      }
      else if(e.keyCode==66){
+      document.getElementById('66').classList.add("playing");
         var x = document.getElementById('66');
         x.play();
      }
      else if(e.keyCode==67){
+      document.getElementById('67').classList.add("playing");
         var x = document.getElementById('67');
         x.play();
      }
      else if(e.keyCode==68){
+      document.getElementById('68').classList.add("playing");
         var x = document.getElementById('68');
         x.play();
      }
